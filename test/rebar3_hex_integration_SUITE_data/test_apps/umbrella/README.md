5456dc423b474e272ffc82e5315903ab5ee41939
Bryan Paxton <bryan@starbelly.io>
2021-09-05 11:16:14 -0500

    Merge docs and revert tasks into publish task
    - Removed rebar3_hex_docs and rebar3_hex_revert
    - Added rebar3_hex_build
    - Added support for umbrella projects in tests with stub umbrella project to boot

---
+++ test/rebar3_hex_integration_SUITE_data/test_apps/umbrella/README.md
@@ -0,0 +1,9 @@
+umbrella
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ test/rebar3_hex_integration_SUITE_data/test_apps/umbrella/apps/umbrella/src/umbrella_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc umbrella public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(umbrella_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    umbrella_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
